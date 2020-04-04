5a14aad6c7afdd5bbedf2e1cef5da895b6133fa5
kr4mpu5 <derkrampus@icloud.com>
2020-04-04 00:24:08 -0500

    Updates

---
+++ precache-manifest.2a8f08a117c310c6051d3243c074fcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3954aecff4b542dfbd3cc7069f4afe",
+    "url": "/hw-20/index.html"
+  },
+  {
+    "revision": "8fe6ed7ed111a82be7ee",
+    "url": "/hw-20/static/css/main.4ed0bcdb.chunk.css"
+  },
+  {
+    "revision": "3dfd2847ec988dc5f95d",
+    "url": "/hw-20/static/js/2.7b91c02a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw-20/static/js/2.7b91c02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe6ed7ed111a82be7ee",
+    "url": "/hw-20/static/js/main.74b27de0.chunk.js"
+  },
+  {
+    "revision": "345493359c5eea535711",
+    "url": "/hw-20/static/js/runtime-main.f1500bad.js"
+  }
+]);
